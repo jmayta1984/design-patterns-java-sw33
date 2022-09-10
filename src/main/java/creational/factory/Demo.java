bca3b50218ded87dd9196f0ae3ce4d753d3b76f2
Jorge Mayta <jmayta@pucp.edu.pe>
2022-09-10 12:30:41 -0500

    initial commit

---
+++ src/main/java/creational/factory/Demo.java
@@ -0,0 +1,17 @@
+package creational.factory;
+
+public class Demo {
+
+    public static void main(String[] args){
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        Shape circle = shapeFactory.getShape("CIRCLE");
+
+        circle.draw();
+
+        Shape rectangle = shapeFactory.getShape("RECTANGLE");
+
+        rectangle.draw();
+
+    }
+}
